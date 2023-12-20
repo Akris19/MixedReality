34fbed396e35e7a7d12b0a364b1ee052cfb767d4
Anders Risager Kristensen <akris19@student.sdu.dk>
2023-12-20 03:11:42 +0100

    Update

--- Binoculars - Økolariet/Assets/_Scripts/Helpers.cs
+++
@@ -1,8 +0,0 @@
-
-public static class Helpers
-{
-    public static int Map(int value, int fromLow, int fromHigh, int toLow, int toHigh)
-    {
-        return (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
-    }
-}
